1934bd8cb236f87df9e09d629285c644274bd3ce
otknoy <otknoy@gmail.com>
2012-04-18 02:24:36 +0900

    change using search method once

--- imgDown.rb
+++ imgDown.rb
@@ -31,13 +31,8 @@ Dir::mkdir out_dir unless File.exists? out_dir
 ARGV.each do |http|
   doc = Hpricot(open(http))
   
-  (doc/:a).each do |a|
+  ((doc/:a) | (doc/:img)).each do |a|
     uri = a[:href]
-    save_file(uri, out_dir) if uri =~ /.*\.(?:jpg|jpeg|png)/
-  end
-
-  (doc/:img).each do |img|
-    uri = img[:src]
     save_file(uri, out_dir) if uri =~ /.*\.(?:jpg|jpeg|png)/
   end
 end
